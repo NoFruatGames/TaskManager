abf3fa12ffdd6b54086fa63325a270c8153ecfb8
NoFruatGames <gamercombat57@gmail.com>
2023-09-12 13:44:24 +0300

    added Server class

--- TMServer/Program.cs
+++ TMServer/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TMServer;
+
+Server gameServer = new Server("192.168.0.129", 4980);
+try
+{
+    gameServer.Start();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    gameServer.Dispose();
+}
---
+++ TMServer/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TM_Server;
+
+namespace TMServer
+{
+    internal class Server :IDisposable
+    {
+        private ConnectionHandler connectionHandler;
+        public Server(string host, int port)
+        {
+            connectionHandler = new ConnectionHandler(new IPEndPoint(IPAddress.Parse(host), port));
+            connectionHandler.ClientConnected += ConnectionHandler_ClientConnected; ;
+        }
+        public void Start()
+        {
+            try
+            {
+                connectionHandler.StartListener();
+                Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: Server started at {connectionHandler.EndPoint.Address}:{connectionHandler.EndPoint.Port}");
+                Thread listenClientsThread = new Thread(connectionHandler.ListenClients);
+                listenClientsThread.Name = "connection listener thread";
+                listenClientsThread.Start();
+                while (true)
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+        private void ConnectionHandler_ClientConnected(TcpClient obj)
+        {
+            Console.WriteLine("Client connected");
+            Stream stream = obj.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine("response from server");
+            writer.Flush();
+        }
+        public void Dispose()
+        {
+            connectionHandler.Dispose();
+        }
+    }
+    
+}
